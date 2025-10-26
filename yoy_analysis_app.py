bd479a56e47c1826bf0907a2d9bea576f8199a95
obimax <moloi.mpho.zar@gmail.com>
2025-10-26 23:12:40 +0200

    Remove unused matplotlib import to clean up code

--- yoy_analysis_app.py
+++ yoy_analysis_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from docx import Document
 from docx.shared import Inches, Pt
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
